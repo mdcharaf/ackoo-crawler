8d4a46fa83af1134f84565986dcbe7e1cb771a06
Muhammad Charaf <muhammad.charaf@gmail.com>
2022-01-29 22:32:50 +0200

    build productStore

---
+++ ackoo-partner-crawler/db/models/productModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  _id: String,
+  title: String,
+  partner: String,
+  url: String,
+  media: Array,
+  price: Number,
+  inStock: Boolean,
+});
+
+const ProductModel = mongoose.model('Product', ProductSchema);
+
+module.exports = { ProductModel }
