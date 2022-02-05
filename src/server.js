dd5458cfe78a570a3c9852a603f3bfd5b023bf62
Muhammad Charaf <muhammad.charaf@gmail.com>
2022-02-06 00:02:25 +0200

    wip

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
 
 const fetch = require('node-fetch')
 const ProductStore = require('./store/product')
-const { makeHmCrawler } = require('./crawlers/hm');
+const { makeHmCrawler } = require('./crawlers/hm')
 
-(async () => {
-  try {
-    const crawler = makeHmCrawler({ fetch, store: ProductStore })
-    await crawler.crawl()
+module.exports = {
+  serve: async () => {
+    try {
+      const crawler = makeHmCrawler({ fetch, store: ProductStore })
+      await crawler.crawl()
 
-    console.log('finished')
-  } catch (error) {
-    console.log(error)
-  } finally {
-    process.exit()
+      console.log('finished')
+    } catch (error) {
+      console.log(error)
+    }
   }
-})()
+}
