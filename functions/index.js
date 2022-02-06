d4f8bdd8d1b94dee366df89b63e88ecd1a1f22d9
Muhammad Charaf <muhammad.charaf@gmail.com>
2022-02-06 17:37:15 +0200

    crawl function firebase

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,6 @@
 const functions = require('firebase-functions')
 const { crawl } = require('./app/server')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info('Hello logs!', { structuredData: true })
   response.send('Hello from Firebase!')
@@ -13,7 +10,9 @@ exports.crawl = functions.https.onRequest(async (request, response) => {
   try {
     await crawl()
     response.send('Success')
+    functions.logger.info('Successfully crawled data', { structuredData: true })
   } catch (error) {
+    functions.logger.error('Hello logs!', { structuredData: true })
     response.send(JSON.stringify(error))
   }
 })
